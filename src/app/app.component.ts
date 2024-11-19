4e24ecce4627c167ec96ef9a111356b85394c406
DaniGarcia10 <danielgarciapelaez@gmail.com>
2024-11-19 23:24:14 +0100

    Prueba CI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TasklistComponent } from './components/tasklist/tasklist.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,TasklistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/models/task.model.ts
@@ -0,0 +1,62 @@
+export enum TaskStatus{
+    COMPLETED="C",
+    IN_PROGRESS="IP",
+    PENDING="P"
+}
+
+export enum TaskPriority{
+    HIGH="H",
+    MEDIUM="M",
+    LOW="L"
+}
+
+export class Task {
+    id:number;
+    name:string;
+    description:string;
+    priority:TaskPriority;
+    status:TaskStatus;
+    creationDate:Date;
+    expirationDate:Date;
+    isDelete:boolean;
+
+    constructor (id:number,name:string,description:string,priority:TaskPriority,status:TaskStatus,creationDate:Date,expirationDate:Date,isDelete:boolean){
+        this.id = id;
+        this.name = name;
+        this.description=description;
+        this.priority=priority;
+        this.status = status;
+        this.creationDate = creationDate;
+        this.expirationDate =expirationDate;
+        this.isDelete = isDelete;
+    }
+
+    getStatusText():string{
+        let text="";
+        switch(this.status){
+            case "IP": text="En proceso";break;
+            case "C": text="Realizada"; break;
+            case "P": text="Pendiente";break;
+        }
+        return text;
+    }
+
+    getPriorityText():string{
+        switch(this.priority){
+            case "H": return "Alta";break;
+            case "M": return "Media"; break;
+            case "L": return "Baja";break;
+            default: return "";
+        }
+    }
+
+    getPriorityColor(){
+        switch(this.priority){
+            case "H": return "red";break;
+            case "M": return "blue"; break;
+            case "L": return "black";break;
+            default: return "";
+        }
+    }
+
+}
